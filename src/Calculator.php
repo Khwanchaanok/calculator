b5d9cf0a539d587f124bfeaeadc60db8c6906b8a
Khwanchanok <Khwanchanoka57@email.nu.ac.th>
2017-05-29 16:00:26 +0700

    update class file

---
+++ src/Calculator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Khwanchanok\Calculator;
+
+class Calculator
+{
+    public function add($first_num , $secound_num)
+    {
+        return $first_num + $secound_num;
+    }
+     public function subtrack($first_num , $secound_num)
+    {
+        return $first_num - $secound_num;
+    }
+     public function multiply($first_num , $secound_num)
+    {
+        return $first_num * $secound_num;
+    }
+     public function devide($first_num , $secound_num)
+    {
+        return $first_num / $secound_num;
+    }
+}
